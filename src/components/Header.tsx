507a099bd2be2600dfdd95e5cc84562e1681f473
/sam <Samuelfisklug@gmail.com>
2023-09-16 16:34:57 -0300

    🔨 ┇ Componente adicionado

---
+++ src/components/Header.tsx
@@ -0,0 +1,34 @@
+import Image from "next/image";
+import Logo from "@/img/logo.png";
+
+import { ItemMenu } from "./ItemMenu";
+import { Container } from "./Container";
+import { Modal } from "./Modal";
+import Link from "next/link";
+
+export function Header() {
+  return (
+    <header className="flex items-center w-full h-20 bg-primary-red">
+      <div className="max-w-[1246px] px-[15px] mx-auto">
+        <Container>
+          <div className="flex items-center gap-14">
+            <ul className="flex items-center gap-12">
+              <li>
+                <ItemMenu name="Comprar" />
+              </li>
+              <li>
+                <Link href={"/"}>
+                <Image src={Logo} alt="Logo" width={150} />
+                </Link>
+              </li>
+              <li>
+                <ItemMenu name="Vender" />
+              </li>
+            </ul>
+          </div>
+        </Container>
+        {/* <Modal isVisible/> */}
+      </div>
+    </header>
+  );
+}
---
+++ src/components/Modal.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+type Props = {
+  isVisible: boolean;
+};
+
+export function Modal({ isVisible }: Props) {
+  if ( !isVisible ) return null;
+  return (
+    <div className="fixed inset-0 bg-black bg-opacity-25 backdrop-blur-sm flex justify-center items-center">
+      <div className="w-[600px] flex flex-col">
+        <button className="text-white text-xl place-self-end">X</button>
+        <div className="bg-white p-2 rounded">Modal</div>
+      </div>
+    </div>
+  );
+}
