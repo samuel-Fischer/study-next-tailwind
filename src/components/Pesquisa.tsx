46d5746d9212d60fa6e4c9bd7fdb69cb70d439c0
/sam <Samuelfisklug@gmail.com>
2023-09-19 09:02:01 -0300

    🔧 ┇ buttons

--- src/components/Pesquisa.tsx
+++ src/components/Pesquisa.tsx
@@ -1,3 +1,4 @@
+import { ListRestart, MoveDown, Search } from "lucide-react";
 import { useForm } from "react-hook-form";
 
 export default function Pesquisa(props: any) {
@@ -9,20 +10,30 @@ export default function Pesquisa(props: any) {
       onSubmit={handleSubmit(props.filtrarCarros)}
       onReset={props.mostrarTodos}
     >
-      <div className="form-group">
-        <input
-          type="text"
-          className="form-control"
-          placeholder="Pesquisar"
-          {...register("pesq")}
-        />
+      <div className="m-5 grid grid-cols-1 grid-flow-col">
+        <div className="form-group">
+          <input
+            type="text"
+            className="form-control shadow-md rounded-md p-2"
+            placeholder="Pesquisar"
+            {...register("pesq")}
+          />
+        </div>
+        <div className="grid grid-cols-1 grid-flow-col gap-4">
+          <button type="submit" className="shadow-md rounded-md p-2">
+            <Search />
+          </button>
+          <button type="button" className="bg-slate-100 hover:bg-slate-50 p-2" onClick={props.orderByPreco}>
+            <MoveDown />
+          </button>
+          <button type="reset" className="bg-slate-100 hover:bg-slate-50 p-2">
+            <ListRestart />
+          </button>
+          {/* <button type="reset" className="">
+            Limpar
+          </button> */}
+        </div>
       </div>
-      <button type="submit" className="btn btn-primary">
-        Pesquisar
-      </button>
-      <button type="reset" className="btn btn-primary">
-        Limpar
-      </button>
     </form>
   );
 }
