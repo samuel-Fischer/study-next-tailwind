ab16b620058ec11ac4b27acdf2b52cf5f45ab312
/sam <Samuelfisklug@gmail.com>
2023-09-18 23:30:27 -0300

    🚧 ┇ under construction

--- src/app/comprar/page.tsx
+++ src/app/comprar/page.tsx
@@ -1,4 +1,4 @@
-export default function vender() {
+export default function Comprar() {
   return (
     <div className="bg-primary-gray">
       <h1 className="font-bold text-primary-gray text-2xl">Compra</h1>
