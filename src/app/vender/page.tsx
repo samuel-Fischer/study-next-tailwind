692de08428b61eac70022160e998f670d28cbff8
Samuel Fischer <samuelfisklug@gmail.com>
2023-11-26 12:49:52 -0300

    Fix typo in year field name

--- src/app/vender/page.tsx
+++ src/app/vender/page.tsx
@@ -5,7 +5,7 @@ import { useForm } from "react-hook-form";
 type Inputs = {
   marca: string;
   quilometragem: number;
-  ano: number;
+  ano_id: number;
   preco: number;
   sobre: string;
   imagem: string;
@@ -28,12 +28,13 @@ export default function Cadastro() {
 
   async function onSubmit(data: Inputs) {
     console.log(data);
+    console.log(typeof data.ano_id);
     const carros = await fetch("http://localhost:3004/carros", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({ ...data, preco: Number(data.preco), quilometragem: Number(data.quilometragem)}),
+      body: JSON.stringify({ ...data, preco: Number(data.preco), quilometragem: Number(data.quilometragem), ano_id: Number(data.ano_id)}),
     });
     if (carros.status === 201) {
       alert("Cadastro realizado com sucesso!");
@@ -187,10 +188,12 @@ export default function Cadastro() {
             <select
               className="valid:border-green-500 valid:text-green-600"
               id="ano"
-              {...register("ano", { required: true })}
+              
+              {...register("ano_id", { required: true })}
             >
               {anos.map((ano) => (
-                <option key={ano.id} value={ano.ano}>
+                  console.log(ano.id),
+                <option key={ano.id} value={ano.id}>
                   {ano.ano}
                 </option>
               ))}
